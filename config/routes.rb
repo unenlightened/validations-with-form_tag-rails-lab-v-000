3502c07038f9f5138e2fb9ea2cf0253e6d72c5f3
Chely Ho <adept_inept@yahoo.com>
2018-04-10 19:29:31 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :authors, only: [:show, :new, :create]
+  resources :posts, only: [:show, :edit, :update]
 end
