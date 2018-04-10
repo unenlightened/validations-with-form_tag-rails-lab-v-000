e7859097cda3746fa97964ee2b081111b73bc669
Chely Ho <adept_inept@yahoo.com>
2018-04-10 19:16:44 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :authors, only: [:show, :new, :edit, :create, :update]
+  resources :posts, only: [:show, :new, :edit, :create, :update]
 end
